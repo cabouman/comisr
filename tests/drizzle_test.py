f0ede12d1ba31fbe2c8ad80f83fb0a82c413ef4c
Yufang Sun <ysun@Zhenhaos-MacBook-Pro.local>
2024-01-12 12:49:20 -0600

    add frame numbers

--- tests/drizzle_test.py
+++ tests/drizzle_test.py
@@ -13,6 +13,9 @@ import image_processing
 from scipy.signal import convolve2d as conv2
 from skimage import color, data, restoration
 
+# define number of frames
+frames_number = 4
+
 # 1. Read in a high resolution image
 img = cv2.imread('./input/my_video_frame30.png', 0) 
 
@@ -21,15 +24,19 @@ img = cv2.imread('./input/my_video_frame30.png', 0)
 print('Image Shape:', m, n) 
 
 # Crop image with different offset
-img1 = image_processing.crop_image(img, offset_max = 8)
-img2 = image_processing.crop_image(img, offset_max = 8)
-img3 = image_processing.crop_image(img, offset_max = 8)
-img4 = image_processing.crop_image(img, offset_max = 8)
+img1 = np.zeros(shape=(frames_number,600,1080), dtype=img.dtype)
 
-[width, height] = img1.shape
+for i in range(frames_number):
+    img1[i] = image_processing.crop_image(img, offset_max = 8)
+    #img1 = image_processing.crop_image(img, offset_max = 8)
+    #img2 = image_processing.crop_image(img, offset_max = 8)
+    #img3 = image_processing.crop_image(img, offset_max = 8)
+    #img4 = image_processing.crop_image(img, offset_max = 8)
+
+[width, height] = img1[0].shape
 print('cropped image size:', width,height)
 
-cv2.imwrite('img1_cropped.png', img1) 
+cv2.imwrite('img1_cropped.png', img1[0]) 
 #cv2.imshow("cropped", img1)
 #cv2.waitKey(0)
 
@@ -47,46 +54,36 @@ cv2.imwrite('img1_cropped.png', img1)
 # resize image
 f = 2
 scale_percent = 1/f # percent of original size
-width = int(img1.shape[1] * scale_percent)
-height = int(img1.shape[0] * scale_percent)
+width = int(img1[0].shape[1] * scale_percent)
+height = int(img1[0].shape[0] * scale_percent)
 dim = (width, height)
-img1_down = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)
-img2_down = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
-img3_down = cv2.resize(img3, dim, interpolation = cv2.INTER_AREA)
-img4_down = cv2.resize(img4, dim, interpolation = cv2.INTER_AREA)
 
-img2_down_align = image_processing.image_align(img2_down, img1_down)
-img3_down_align = image_processing.image_align(img3_down, img1_down)
-img4_down_align = image_processing.image_align(img4_down, img1_down)
+img_down = np.zeros(shape=(frames_number, height,width), dtype=img.dtype)
+img_down_align = np.zeros(shape=(frames_number, height,width), dtype=img.dtype)
 
+for i in range(frames_number):
+    img_down[i] = cv2.resize(img1[i], dim, interpolation = cv2.INTER_AREA)
+    img_down_align[i] = image_processing.image_align(img_down[i], img_down[0])
 
-cv2.imwrite('img1_down.png', img1_down) 
-cv2.imwrite('img2_down.png', img2_down) 
-cv2.imwrite('img3_down.png', img3_down) 
-cv2.imwrite('img4_down.png', img4_down) 
-
-cv2.imwrite('img2_down_align.png', img2_down_align) 
-cv2.imwrite('img3_down_align.png', img3_down_align) 
-cv2.imwrite('img4_down_align.png', img4_down_align) 
 
 # drizzle image
 start_time = time.time()
-[img_up1, areamap] = image_processing.drizzle_trio(img1_down, f=2, a = 0.5, weight = 2)
+[img_up1, areamap] = image_processing.drizzle_trio(img_down[0], f=2, a = 0.5, weight = 2)
 print("---The running time for up sampling is %s seconds ---" % (time.time() - start_time))
 
-[img_up2, areamap] = image_processing.drizzle_trio(img2_down, f=2, a = 0.5, weight = 2)
-[img_up3, areamap] = image_processing.drizzle_trio(img3_down, f=2, a = 0.5, weight = 3)
-[img_up4, areamap] = image_processing.drizzle_trio(img4_down, f=2, a = 0.5, weight = 4)
+[img_up2, areamap] = image_processing.drizzle_trio(img_down[1], f=2, a = 0.5, weight = 2)
+[img_up3, areamap] = image_processing.drizzle_trio(img_down[2], f=2, a = 0.5, weight = 3)
+[img_up4, areamap] = image_processing.drizzle_trio(img_down[3], f=2, a = 0.5, weight = 4)
 
 img_drizzle = (img_up1 + img_up2 + img_up3 + img_up4)/4
 cv2.imwrite('img_drizzle.png', img_drizzle) 
 
 start_time = time.time()
-[img_drizzle2, areamap, weightmap] = image_processing.drizzle(img1_down, img2_down_align, f=2, a = 0.5, weight = 1, I = img_up1)
+[img_drizzle2, areamap, weightmap] = image_processing.drizzle(img_down[0], img_down_align[1], f=2, a = 0.5, weight = 1, I = img_up1)
 print("---The running time for each drizzle is %s seconds ---" % (time.time() - start_time))
 
-[img_drizzle3, areamap, weightmap] = image_processing.drizzle(img1_down, img3_down_align, f=2, a = 0.5, weight = 1, I = img_drizzle2)
-[img_drizzle4, areamap, weightmap] = image_processing.drizzle(img1_down, img4_down_align, f=2, a = 0.5, weight = 1, I = img_drizzle3)
+[img_drizzle3, areamap, weightmap] = image_processing.drizzle(img_down[0], img_down_align[2], f=2, a = 0.5, weight = 1, I = img_drizzle2)
+[img_drizzle4, areamap, weightmap] = image_processing.drizzle(img_down[0], img_down_align[3], f=2, a = 0.5, weight = 1, I = img_drizzle3)
 
 cv2.imwrite('img_drizzle1.png', img_up1) 
 cv2.imwrite('img_drizzle2.png', img_drizzle2) 
