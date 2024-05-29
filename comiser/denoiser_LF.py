ab92e40bbeb097df6df6f2e93a746fbfae4b14b5
YSUN <ysun@Yufangs-MacBook-Pro.local>
2024-05-29 11:59:04 -0700

    add Gaussian Filter denoiser

--- comiser/denoiser_LF.py
+++ comiser/denoiser_LF.py
@@ -1,6 +1,16 @@
 from skimage import restoration
+from skimage import filters
 
 # Denoiser using  Linear Filter 
+
+def GaussianFilter(image, sigma):
+    # Only set the sigma value; the skimage function will select the optimized filter size that is large enough to effectively 
+    # apply the Gaussian function.
+    
+    filtered_image = filters.gaussian(image, sigma=sigma)
+    return filtered_image
+
+
 def NLM(image, sigma):
     # Use Non-Local Means denoising without the 'multichannel' parameter
     denoised = restoration.denoise_nl_means(image,
@@ -9,4 +19,3 @@ def NLM(image, sigma):
                                             patch_size=7,
                                             patch_distance=11)
     return denoised
-
